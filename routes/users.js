a9382ee3bc43115ccf1ab8bcc8aafa65359373ac
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-07-29 19:07:33 -0400

    more shit

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({ dest: './uploads' });
 
+
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
@@ -56,7 +59,21 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
     		errors
     	});
     } else {
-        console.log('No errors');
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password,
+            profileImage: profileImage
+        });
+
+        User.createUser(newUser, function(err, user) {
+            if(err) throw err
+                console.log(user);
+        });
+
+        res.location('/');
+        res.redirect('/');
     }
 
 });
