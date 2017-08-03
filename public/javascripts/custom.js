5f3bbc596025d081eeccfbc65dcf4906013a8d89
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-08-03 19:49:40 -0400

    pushing the real files

---
+++ public/javascripts/custom.js
@@ -0,0 +1,20 @@
+var buttonScanner = document.getElementById('networkTable');
+var tbody = document.getElementById('tbody');
+
+
+
+//display loading image 
+buttonScanner.onclick = () => {
+
+	var img = document.createElement('IMG');
+	img.setAttribute('src', 'img/wifiLoader.gif');
+	img.setAttribute('id', 'imgLoader')
+	img.setAttribute('width', 304);
+	img.setAttribute('height', 228);
+	tbody.append(img);
+	
+	setTimeout(() =>{
+		document.getElementById('imgLoader').style.display = 'none'
+	}, 3500);
+
+}
